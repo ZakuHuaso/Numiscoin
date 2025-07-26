d1ce7a75eff4ca1ee2a0ac7eb6bb16cd6c5f5986
ZakuHuaso <g.vergara.val@gmail.com>
2025-07-26 16:09:01 -0400

    chore(img): Add new images in Assets

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,23 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-};
+  production: false,
+  firebaseConfig : {
 
+    apiKey: "AIzaSyBkJJ4Hc5l25QlLO3BIeGv4c3FwcD4wb1s",
+  
+    authDomain: "numiscoin-63b4c.firebaseapp.com",
+  
+    projectId: "numiscoin-63b4c",
+  
+    storageBucket: "numiscoin-63b4c.firebasestorage.app",
+  
+    messagingSenderId: "1062463368220",
+  
+    appId: "1:1062463368220:web:f776d1f979f87a140503d9"
+  
+  },  
+};
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
