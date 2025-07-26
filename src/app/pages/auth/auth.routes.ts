8c7b90adbaea819071bb834b78be253dbd477d3a
ZakuHuaso <g.vergara.val@gmail.com>
2025-07-26 16:13:57 -0400

    feat(routes): Routes updated

---
+++ src/app/pages/auth/auth.routes.ts
@@ -0,0 +1,25 @@
+// src/app/features/auth/auth.routes.ts
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./login/login.page').then((m) => m.LoginPage),
+  },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./register/register.page').then((m) => m.RegisterPage),
+  },
+  {
+    path: 'reset-password',
+    loadComponent: () =>
+      import('./reset-password/reset-password.page').then((m) => m.ResetPasswordPage),
+  },
+];
